4eed5c60809498e8dfaf98ba66a8c8082b9f3a91
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-12-20 10:33:25 +0100

    Configured setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BasketIntelligence',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pyspark'
+    ],
+    author=zhenxiay,
+    author_email=yu.zhenxiao.yz@gmail.com,
+    url="https://github.com/zhenxiay/BaseketIntelligence"
+)
