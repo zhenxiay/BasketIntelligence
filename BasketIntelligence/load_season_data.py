5ff5ddbdbdefb656cc678961d781467ee1b6ed90
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-12-22 11:05:21 +0100

    Added method for loading data to big query in the class load season data

--- BasketIntelligence/load_season_data.py
+++ BasketIntelligence/load_season_data.py
@@ -1,16 +1,40 @@
 
 from pyspark.sql import SparkSession
+from google.cloud import bigquery
 
 class LoadSeasonData(CreateSeason):
     def __init__(self,year):
         super().__init__(year=year)
         self.year = year
+        self.project = project
+        self.dataset = dataset
+        self.table_name = table_name
 
     def get_spark():
         spark = SparkSession \
         .builder \
         .appName("BasketIntelligence") \
         .getOrCreate()
+
+    def create_big_query_client():
+        # Construct a BigQuery client object.
+        client = bigquery.Client()
+        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE") # WRITE_APPEND, WRITE_EMPTY
+        return client, job_config
+
+    def load_per_game_to_big_uqery(self):
+        dataset = CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
+        table_id = f'{self.project}.{self.datast}.{self.table_name}'
+        client, job_config = create_big_query_client()
+        job = client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
+        print('Data load to big query successfully!')
+
+    def load_adv_stats_to_big_uqery(self):
+        dataset = CreateSeason(self.year).read_adv_stats_game().drop(columns=['Awards'])
+        table_id = f'{self.project}.{self.datast}.{self.table_name}'
+        client, job_config = create_big_query_client()
+        job = client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
+        print('Data load to big query successfully!')
     
     def load_per_game_to_lakehouse(self):
         spark = self.get_spark()
