7e09712bc15bc9c5f01e4ba1fdad46b385a75ea9
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-12-22 11:48:35 +0100

    Corrected syntax in the class LoadSeasonData

--- BasketIntelligence/load_season_data.py
+++ BasketIntelligence/load_season_data.py
@@ -1,44 +1,46 @@
 
 from pyspark.sql import SparkSession
 from google.cloud import bigquery
+from BasketIntelligence.create_season import CreateSeason
 
-class LoadSeasonData(create_season):
-    def __init__(self,year):
+class LoadSeasonData(CreateSeason):
+    def __init__(self, year, project, dataset, table_name):
         super().__init__(year=year)
-        self.year = year
         self.project = project
         self.dataset = dataset
         self.table_name = table_name
 
+    @staticmethod
     def get_spark():
         spark = SparkSession \
-        .builder \
-        .appName("BasketIntelligence") \
-        .getOrCreate()
+            .builder \
+            .appName("BasketIntelligence") \
+            .getOrCreate()
+        return spark
 
+    @staticmethod
     def create_big_query_client():
-        # Construct a BigQuery client object.
         client = bigquery.Client()
-        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE") # WRITE_APPEND, WRITE_EMPTY
+        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
         return client, job_config
 
-    def load_per_game_to_big_uqery(self):
-        dataset = create_season.CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
-        table_id = f'{self.project}.{self.datast}.{self.table_name}'
-        client, job_config = create_big_query_client()
+    def load_per_game_to_big_query(self):
+        dataset = CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
+        table_id = f'{self.project}.{self.dataset}.{self.table_name}'
+        client, job_config = self.create_big_query_client()
         job = client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
-        print('Data load to big query successfully!')
+        print(f'Data load to big query {table_id} successfully!')
 
-    def load_adv_stats_to_big_uqery(self):
-        dataset = create_season.CreateSeason(self.year).read_adv_stats_game().drop(columns=['Awards'])
-        table_id = f'{self.project}.{self.datast}.{self.table_name}'
-        client, job_config = create_big_query_client()
+    def load_adv_stats_to_big_query(self):
+        dataset = CreateSeason(self.year).read_adv_stats_game().drop(columns=['Awards'])
+        table_id = f'{self.project}.{self.dataset}.{self.table_name}'
+        client, job_config = self.create_big_query_client()
         job = client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
-        print('Data load to big query successfully!')
+        print(f'Data load to big query {table_id} successfully!')
     
     def load_per_game_to_lakehouse(self):
         spark = self.get_spark()
-        dataset = create_season.CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
+        dataset = CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
         dataset_spark = spark.createDataFrame(dataset)
 
         drop_action = spark.sql(f"DROP TABLE IF EXISTS basketball_reference_per_game_{self.year}")
@@ -49,7 +51,7 @@ class LoadSeasonData(create_season):
     
     def load_adv_stats_to_lakehouse(self):
         spark = self.get_spark()
-        dataset = create_season.CreateSeason(self.year).read_adv_stats().drop(columns=['Awards'])
+        dataset = CreateSeason(self.year).read_adv_stats().drop(columns=['Awards'])
         dataset_spark = spark.createDataFrame(dataset)
         
         drop_action = spark.sql(f"DROP TABLE IF EXISTS basketball_reference_adv_stats_{self.year}")
