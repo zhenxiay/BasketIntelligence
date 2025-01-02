c801bf3a0eb3deb02b9f025ec6ca297d305f010e
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-02 15:00:24 +0100

    Update test_create_season.py
    Add function to test reading data from team adv stats

--- Tests/test_create_season.py
+++ Tests/test_create_season.py
@@ -1,1 +1,7 @@
+from BasketIntelligence.create_season import CreateSeason
 
+def test_read_team_adv_stats():
+  
+  dataset = CreateSeason("2025")
+  columns_count = len(dataset.read_stats_per_game().columns)
+  assert columns_count == 28
