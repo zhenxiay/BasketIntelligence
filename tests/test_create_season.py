b662d61502ae2e4c1d3314088649a99cf40525bf
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-02 15:50:25 +0100

    Update test_create_season.py
    Corrected typos in the function that is to be tested

--- tests/test_create_season.py
+++ tests/test_create_season.py
@@ -3,7 +3,7 @@ from BasketIntelligence.create_season import CreateSeason
 def test_read_team_adv_stats():
   
   dataset = CreateSeason("2025")
-  columns_count = len(dataset.read_team_adv_game().columns)
+  columns_count = len(dataset.read_team_adv_stats().columns)
   assert columns_count == 28
 
 def test_read_stats_per_game():
