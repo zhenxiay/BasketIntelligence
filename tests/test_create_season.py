da5fd5e03c33b5570de7ec86fd0a7d35e181254b
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-07 11:28:43 +0100

    Update test_create_season.py - adjust columns count as new colum has been added to each data frame

--- tests/test_create_season.py
+++ tests/test_create_season.py
@@ -4,16 +4,16 @@ def test_read_team_adv_stats():
   
 	dataset = CreateSeason("2025")
 	columns_count = len(dataset.read_team_adv_stats().columns)
-	assert columns_count == 28
+	assert columns_count == 29
 
 def test_read_stats_per_game():
     
 		dataset = CreateSeason("2025")
 		columns_count = len(dataset.read_stats_per_game().columns)
-		assert columns_count == 31
+		assert columns_count == 32
 
 def test_read_team_shooting():
 		dataset = CreateSeason("2025")
 		columns_count = len(dataset.read_team_shooting().columns)
-		assert columns_count == 15
+		assert columns_count == 16
     
