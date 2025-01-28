ac4d6682e771a059317dfe6c91106935924bf8fb
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-28 10:04:18 +0100

    Corrected syntax error

--- BasketIntelligence/load_season_data.py
+++ BasketIntelligence/load_season_data.py
@@ -18,7 +18,7 @@ class LoadSeasonData(CreateSeason):
         return spark
 
     @staticmethod
-    def data_ingestion_lakehouse(dataset,name): -> None
+    def data_ingestion_lakehouse(dataset,name) -> None:
         spark = self.get_spark()
         dataset_spark = spark.createDataFrame(dataset)
         
@@ -35,40 +35,40 @@ class LoadSeasonData(CreateSeason):
         return client, job_config
 
     @staticmethod
-    def data_ingestion_big_query(dataset,table_name): -> None
+    def data_ingestion_big_query(dataset,table_name) -> None:
         table_id = f'{self.project}.{self.dataset_name}.{table_name}'
         client, job_config = self.create_big_query_client()
         client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
         print(f'Data load to big query {table_id} successfully!')
 
-    def load_per_game_to_big_query(self,table_name): -> None
+    def load_per_game_to_big_query(self,table_name) -> None:
         dataset = CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
         self.data_ingestion_big_query(dataset,table_name)
 
-    def load_adv_stats_to_big_query(self,table_name): -> None
+    def load_adv_stats_to_big_query(self,table_name) -> None:
         dataset = CreateSeason(self.year).read_adv_stats().drop(columns=['Awards'])
         data_ingestion_big_query(dataset,table_name)
 
-    def load_team_adv_stats_to_big_query(self,table_name): -> None
+    def load_team_adv_stats_to_big_query(self,table_name) -> None:
         dataset = CreateSeason(self.year).read_team_adv_stats()
         data_ingestion_big_query(dataset,table_name)
         
-    def load_team_shooting_to_big_query(self,table_name): -> None
+    def load_team_shooting_to_big_query(self,table_name) -> None:
         dataset = CreateSeason(self.year).read_team_shooting()
         data_ingestion_big_query(dataset,table_name)
     
-    def load_per_game_to_lakehouse(self): -> None
+    def load_per_game_to_lakehouse(self) -> None:
         dataset = CreateSeason(self.year).read_stats_per_game().drop(columns=['Awards'])
         self.data_ingestion_lakehouse(dataset,'per_game')
     
-    def load_adv_stats_to_lakehouse(self): -> None
+    def load_adv_stats_to_lakehouse(self) -> None:
         dataset = CreateSeason(self.year).read_adv_stats().drop(columns=['Awards'])
         self.data_ingestion_lakehouse(dataset,'adv_stats')
 
-    def load_team_adv_stats_to_lakehouse(self): -> None
+    def load_team_adv_stats_to_lakehouse(self) -> None:
         dataset = CreateSeason(self.year).read_team_adv_stats()
         self.data_ingestion_lakehouse(dataset,'team_adv_stats')
         
-    def load_team_shooting_to_lakehouse(self): -> None
+    def load_team_shooting_to_lakehouse(self) -> None:
         dataset = CreateSeason(self.year).read_team_shooting()
         self.data_ingestion_lakehouse(dataset,'team_shooting')
