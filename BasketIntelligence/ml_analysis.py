bfb44d16795bc3a65c7b4735c9446a5cab6a128c
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-06-06 11:33:39 +0200

    Update ml_analysis.py

--- BasketIntelligence/ml_analysis.py
+++ BasketIntelligence/ml_analysis.py
@@ -23,7 +23,7 @@ def k_means_player_clustering(year, n_clusters):
     dataset = CreateSeason(year).read_adv_stats().drop(columns=['Awards'])
         
     label_cols = ['Rk','Player','Pos','Age','Team']
-    k_means_cols = ['TS_Rate','3PAr','FTr', 'ORB_Rate', 'DRB_Rate', 'AST_Rate', 'STL_Rate', 'BLK_Rate', 'TOV_Rate', 'USG%']
+    k_means_cols = ['TS_rate','3PAr','FTr', 'ORB_rate', 'DRB_rate', 'AST_rate', 'STL_rate', 'BLK_rate', 'TOV_rate', 'USG_rate']
     output_cols = ['Rk','Player','Pos','Age','Team', 'kMeans']
   
     X = dataset[k_means_cols]
