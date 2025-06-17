4181f90ca814f28da6e25b2cdafc0eed2b693bc4
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-06-17 16:49:56 +0200

    Use argparse to enable the file data_ingestion_ml_data_to_Big_Query.py to take param values from outside

--- DataLoad/data_ingestion_ml_data_to_Big_Query.py
+++ DataLoad/data_ingestion_ml_data_to_Big_Query.py
@@ -1,13 +1,38 @@
 from BasketIntelligence.load_season_data import LoadSeasonData
+import argparse
+
+parser = argparse.ArgumentParser(description ='Define args for GitHub actions')
+
+parser.add_argument('year',
+                    type = string,
+                    help ='Year of the NBA season.')
+
+parser.add_argument('GCP_Name',
+                   type = string,
+                   help ='Google cloud project name.')
+
+parser.add_argument('dataset',
+                   type = string,
+                   help ='Google Big Query dataset name.')
+
+parser.add_argument('table_name',
+                   type = string,
+                   help ='Google Big Query table name to which the data will be ingested to.')
+
+parser.add_argument('n_cluster',
+                   type = integer,
+                   help ='Number of kmeans clusters that is to be applied.')
+
+args = parser.parse_args()
 
 def main():
 
-    dataset = LoadSeasonData("2025",
-                         "keen-vial-420113",
-                         "BasketIntelligence")
+    dataset = LoadSeasonData(args.year,
+                             args.GCP_Name,
+                             args.dataset)
 
-    dataset.load_kmeans_team_shooting_to_big_query(table_name="kmeans_team_shooting",
-                                                   n_cluster=5)
+    dataset.load_kmeans_team_shooting_to_big_query(table_name=args.table_name,
+                                                   n_cluster=args.n_cluster)
 
 if __name__ == "__main__":
     main()
