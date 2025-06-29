2029791a05f9839cfc3bf78e6b6770293ac98f89
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-06-29 21:11:49 +0200

    Update test_load_data_to_sqlite.py

--- tests/pyTest/test_load_data_to_sqlite.py
+++ tests/pyTest/test_load_data_to_sqlite.py
@@ -1,5 +1,11 @@
-from BasketIntelligence.load_season_data import load_per_game_to_sqlite
+from BasketIntelligence.load_season_data import LoadSeasonData
 
 def test_load_per_game_to_sqlite():
+
+    dataset = LoadSeasonData("2025","project","BasketIntelligence")
     
-	assert load_per_game_to_sqlite() is true
+    table_name = "per_game"
+    db_path = "./"
+    db_name = "test_db"
+    
+	assert dataset.load_per_game_to_sqlite(table_name, db_path, db_name) is true
