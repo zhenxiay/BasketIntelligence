e6ec0013d453f588b63f43cf7d068703f5b1fc0d
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2025-07-01 17:21:39 +0200

    Update docstring in connect_sqlite module for clarity

--- BasketIntelligence/utils/connect_sqlite.py
+++ BasketIntelligence/utils/connect_sqlite.py
@@ -1,4 +1,10 @@
+'''
+Define a module to connect to a SQLite database.
+This module is to be used by the `LoadSeasonData` class for data ingestion.
+
+'''
 import sqlite3
 
+# define the function to create & connect to a SQLite database
 def connect_sqlite(db_path, db_name):        
     return sqlite3.connect(f'{db_path}/{db_name}')
