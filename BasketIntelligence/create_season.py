de37f64e63791dd00fbddd26cd4d4b8f2c116e95
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2025-07-03 11:27:42 +0200

    Add .gitignore file and improve docstrings in CreateSeason class

--- BasketIntelligence/create_season.py
+++ BasketIntelligence/create_season.py
@@ -3,17 +3,24 @@ import pandas as pd
 from BasketIntelligence.utils.logger import get_logger
 
 class CreateSeason():
+    '''
+    This class is used to create a data object for a specific NBA season.
+    This object contains methods to read and process data from basketball-reference.com.
+    '''
 
     def __init__(self, year):
         self.year = year
-        self.url_per_game = f'https://www.basketball-reference.com/leagues/NBA_{self.year}_per_game.html'
-        self.url_adv_stats = f'https://www.basketball-reference.com/leagues/NBA_{self.year}_advanced.html'
-        self.url_team_stats = f'https://www.basketball-reference.com/leagues/NBA_{self.year}.html'
+        self.base_url = 'https://www.basketball-reference.com/leagues/NBA'
+        self.url_per_game = f'{self.base_url}_{self.year}_per_game.html'
+        self.url_adv_stats = f'{self.base_url}_{self.year}_advanced.html'
+        self.url_team_stats = f'{self.base_url}_{self.year}.html'
         self.logger = get_logger()
-    
+
     @staticmethod
-    # Standard function which is going to be used by the read functions below for data cleaning
     def drop_summary_rows(X):
+        '''
+        Standard function which is used by the read functions below for data cleaning
+        '''
         try:
             X = X.drop(X[X.Team == 'League Average'].index)
         except AttributeError:
@@ -22,44 +29,59 @@ class CreateSeason():
 
     @staticmethod
     def add_season_number(X, season):
+        '''
+        Standard function which is used to add the season number to the dataframe
+        '''
         X['Season'] = season
         return X
-        
-    def read_team_shooting(self):
+
+    def get_dataframe_list(self):
+        '''
+        This function returns a list of dataframes for html page.
+        '''
         list = pd.read_html(self.url_team_stats,
-                          encoding = 'utf-8', 
-                          decimal='.', 
-                          thousands=',')
+                            encoding='utf-8',
+                            decimal='.',
+                            thousands=',')
+        return list
+
+    def read_team_shooting(self):
+        '''
+        This function reads and converts the team shooting stats from the data source.
+        '''
+        list = self.get_dataframe_list()
+
         df_output = list[11]
-        
+
         # preperation for multiindex conversion for affected columns
         df_fg_attempts = df_output.pop('% of FGA by Distance')
         df_fg_pct = df_output.pop('FG% by Distance')
         df_corner_pct = df_output.pop('Corner')
-        
+
         def convert_multi_index(X):
 
-    		# drop first index level 
+    		# drop first index level
             X = X.droplevel(0, axis=1)
 
     		# add converted single index columns back to the output dataframe
             X = X.assign(**{f'FGA-{col}': df_fg_attempts[col] for col in df_fg_attempts.columns})
             X = X.assign(**{f'pct-{col}': df_fg_pct[col] for col in df_fg_pct.columns})
             X = X.assign(**{f'corner-{col}': df_corner_pct[col] for col in df_corner_pct.columns})
-    
-        	# define a function to turn columns for shoot attempts and pct per distance and corner from nested columns to single index columns
+
+            # turn following columns from nested columns to single index columns:
+            # shoot attempts, pct per distance and corner 
             selected_columns = X.filter(regex='(Team|FGA-|pct-|corner-)').columns
             X = X[selected_columns]
 
             return X
-            
+
         def rename_columns(X):
             X.columns = X.columns.str.replace('%', '', regex=False)
             X.columns = X.columns.str.replace('-', '_', regex=False)
-            
-            return X            
-        
-        # shape output dataframe with pipe and the functions defined above         
+
+            return X
+
+        # shape output dataframe with pipe and the functions defined above     
         df_output = (df_output
                 .pipe(convert_multi_index)
                 .pipe(self.drop_summary_rows)
@@ -68,27 +90,29 @@ class CreateSeason():
                 )
 
         self.logger.info(f"Dataframe created with {len(df_output)} rows...")
-        
+
         return df_output
-    
+
     def read_team_adv_stats(self):
-        list = pd.read_html(self.url_team_stats,
-                          encoding = 'utf-8', 
-                          decimal='.', 
-                          thousands=',')
+        '''
+        This function reads and converts the team advanced stats from the data source.
+        '''
+        list = self.get_dataframe_list()
+
         df_output = list[10]
 
         # preperation for multiindex conversion for affected columns
         df_off = df_output.pop('Offense Four Factors')
         df_dev = df_output.pop('Defense Four Factors')
 
-        # define a function to turn columns for offensive and defensive four factors from nested columns to single index columns
         def convert_multi_index(X):
-            # define columns that are to be converted 
+            # define a function to turn following columns from nested columns to single index columns:
+            # for offensive and defensive four factors
+            # define columns that are to be converted
             cols_dev = ['eFG%','TOV%','DRB%','FT/FGA']
             cols_off = ['eFG%','TOV%','ORB%','FT/FGA']
 
-            # drop first index level 
+            # drop first index level
             X = X.droplevel(0, axis=1)
 
             # add converted single index columns back to the output dataframe
@@ -106,8 +130,8 @@ class CreateSeason():
             column_mapping = {'Attend.': 'Attend',
                               'Attend./G': 'Attend_G'
                               }
-            
-            X.rename(columns=column_mapping, 
+
+            X.rename(columns=column_mapping,
                      inplace=True)
 
             X.columns = X.columns.str.replace('%', '_pct', regex=False)
@@ -124,14 +148,15 @@ class CreateSeason():
                    )
 
         self.logger.info(f"Dataframe created with {len(df_output)} rows...")
-        
+
         return df_output
-    
+
     def read_stats_per_game(self):
-        list = pd.read_html(self.url_per_game,
-                          encoding = 'utf-8', 
-                          decimal='.', 
-                          thousands=',')
+        '''
+        This function reads and converts the player per game stats from the data source.
+        '''
+        list = self.get_dataframe_list()
+
         df_output = list[0]
 
         def drop_rows(X):
@@ -169,7 +194,7 @@ class CreateSeason():
                             'PTS': 'float'
                           })
             return X
-        
+
         def rename_columns(X):
             column_mapping = {'3P%': '3P_pct',
                               'FG%': 'FG_pct',
@@ -177,8 +202,8 @@ class CreateSeason():
                               'FT%': 'FT_pct',
                               '2P%': '2P_pct'
                               }
-            
-            X.rename(columns=column_mapping, 
+
+            X.rename(columns=column_mapping,
                      inplace=True)
             return X
 
@@ -190,14 +215,14 @@ class CreateSeason():
                    )
 
         self.logger.info(f"Dataframe created with {len(df_output)} rows...")
-        
+
         return df_output
 
     def read_adv_stats(self):
-        list = pd.read_html(self.url_adv_stats,
-                          encoding = 'utf-8', 
-                          decimal='.', 
-                          thousands=',')
+        '''
+        This function reads and converts the player advanced stats from the data source.
+        '''
+        list = self.get_dataframe_list()
 
         def define_schema(X):
             X = X.astype({
@@ -229,7 +254,7 @@ class CreateSeason():
                           })
             return X
 
-        
+
         def rename_columns(X):
             column_mapping = {'WS/48': 'WS_48',
                               'TS%': 'TS',
@@ -241,16 +266,16 @@ class CreateSeason():
                               'BLK%': 'BLK_rate',
                               'TOV%': 'TOV_rate',
                               'USG%': 'USG_rate'}
-            
-            X.rename(columns=column_mapping, 
+
+            X.rename(columns=column_mapping,
                      inplace=True)
             return X
-        
+
         def drop_rows(X):
             X = X.drop(X[X.Player == 'Player'].index)
             X = X.drop(X[X.Player == 'League Average'].index)
             return X
-        
+
         def filter_rows(X):
             X = X[X['MP'] > 100]
             return X
@@ -264,7 +289,7 @@ class CreateSeason():
                    .pipe(filter_rows)
                    .pipe(self.add_season_number, season = self.year)
                    )
-                
+
         self.logger.info(f"Dataframe created with {len(df_output)} rows...")
 
         return df_output
